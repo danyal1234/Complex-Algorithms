4138c93bcc2ecd28f7cfd5c1de8f8dba8ea2a99f
Danyal Mahmood <dmahmood@mail.uoguelph.ca>
2020-03-18 13:29:29 -0400

    add tree creating (bug)

--- CIS3490_A4/p1.c
+++ CIS3490_A4/p1.c
@@ -5,8 +5,37 @@
 // ****************************************************
 
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "functions.h"
+
+int insertedCount = 1;
+
+typedef struct treeNode TreeNode;
+struct treeNode{
+	char key[52];
+	double averageComparisons;
+	TreeNode* left;
+	TreeNode* right;
+};
+
+TreeNode* createTree (double mainTable[][600], int rootTable[][600], char words[][52], int leftindex, int rightindex) {
+	if (rootTable[leftindex][rightindex] == 0) {
+		return NULL;
+	}
+
+	TreeNode* toReturn = (TreeNode*)malloc(sizeof(TreeNode));
+	strcpy(toReturn->key, words[rootTable[leftindex][rightindex]]);
+	toReturn->averageComparisons = mainTable[leftindex][rightindex];
+
+	insertedCount++;
+
+	toReturn->left = createTree(mainTable, rootTable, words, leftindex, rootTable[leftindex][rightindex]-1);
+	toReturn->right = createTree(mainTable, rootTable, words, rootTable[leftindex][rightindex], rightindex);
+
+	return toReturn;
+}
 
 void OptimalBSTSearch (char words[][52], double probabilities[], int totalUniqueWords) {
 	double mainTable[totalUniqueWords+1][totalUniqueWords];
@@ -14,6 +43,7 @@ void OptimalBSTSearch (char words[][52], double probabilities[], int totalUnique
 	int j = 0;
 	int kmin = 0;
 	double minval = 0;
+	bool minvalset = false;
 	double sum = 0;
 
 	for (int i = 1; i <= totalUniqueWords; ++i) {
@@ -27,9 +57,10 @@ void OptimalBSTSearch (char words[][52], double probabilities[], int totalUnique
 	for (int d = 1; d <= totalUniqueWords-1; ++d) {
 		for (int i = 1; i <= totalUniqueWords-d; ++i) {
 			j = i + d;
-			minval = 2000000000;
+			minvalset = false;
 			for (int k = i; k <= j; ++k) {
-				if (mainTable[i][k-1]+mainTable[k+1][j] < minval) {
+				if (mainTable[i][k-1]+mainTable[k+1][j] < minval || minvalset == false) {
+					minvalset = true;
 					minval = mainTable[i][k-1]+mainTable[k+1][j];
 					kmin = k;
 				}
@@ -44,4 +75,10 @@ void OptimalBSTSearch (char words[][52], double probabilities[], int totalUnique
 		}
 	}
 
+	TreeNode root;
+	strcpy(root.key, words[rootTable[1][totalUniqueWords]]);
+	root.averageComparisons = mainTable[1][totalUniqueWords];
+
+	root.left = createTree(mainTable, rootTable, words, 1, rootTable[1][totalUniqueWords]-1);
+	root.right = createTree(mainTable, rootTable, words, rootTable[1][totalUniqueWords], totalUniqueWords);
 }
